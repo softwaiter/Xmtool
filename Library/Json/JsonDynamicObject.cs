96d7b0ebcf45717e614c72668d766c0da7f1cc98
softwaiter <softwaiter@126.com>
2021-05-20 11:12:59 +0800

    :construction:Json对象增加Path相关操作方法

--- Library/Json/JsonDynamicObject.cs
+++ Library/Json/JsonDynamicObject.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Reflection;
+using System.Text;
 
 namespace CodeM.Common.Tools.Json
 {
@@ -60,9 +62,195 @@ namespace CodeM.Common.Tools.Json
             return true;
         }
 
+        public bool SetValueByPath(string path, object value)
+        {
+            bool bRet = true;
+            string[] pathItems = path.Split('.');
+            dynamic currObj = this;
+            for (int i = 0; i < pathItems.Length; i++)
+            {
+                string item = pathItems[i];
+                if (currObj.Has(item))
+                {
+                    if (i == pathItems.Length - 1)
+                    {
+                        bRet = currObj.TrySetValue(item, value);
+                        break;
+                    }
+
+                    if (!currObj.TryGetValue(item, out currObj))
+                    {
+                        bRet = false;
+                        break;
+                    }
+
+                    if (!(currObj is JsonDynamicObject))
+                    {
+                        bRet = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    bRet = false;
+                    break;
+                }
+            }
+            return bRet;
+        }
+
         public bool Has(string key)
         {
             return mValues.ContainsKey(key);
+        }
+
+        public bool HasPath(string path)
+        {
+            bool bRet = true;
+            string[] pathItems = path.Split('.');
+            dynamic currObj = this;
+            for (int i = 0; i < pathItems.Length; i++)
+            {
+                string item = pathItems[i];
+                if (currObj.Has(item))
+                { 
+                    if (i == pathItems.Length - 1)
+                    {
+                        break;
+                    }
+
+                    if (!currObj.TryGetValue(item, out currObj))
+                    {
+                        bRet = false;
+                        break;
+                    }
+                    
+                    if (!(currObj is JsonDynamicObject))
+                    {
+                        bRet = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    bRet = false;
+                    break;
+                }
+            }
+            return bRet;
+        }
+
+        public bool Remove(string key)
+        {
+            return mValues.Remove(key);
+        }
+
+        public bool RemovePath(string path)
+        {
+            bool bRet = true;
+            string[] pathItems = path.Split('.');
+            dynamic currObj = this;
+            for (int i = 0; i < pathItems.Length; i++)
+            {
+                string item = pathItems[i];
+                if (currObj.Has(item))
+                {
+                    if (i == pathItems.Length - 1)
+                    {
+                        currObj.Remove(item);
+                        break;
+                    }
+
+                    if (!currObj.TryGetValue(item, out currObj))
+                    {
+                        bRet = false;
+                        break;
+                    }
+
+                    if (!(currObj is JsonDynamicObject))
+                    {
+                        bRet = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    bRet = false;
+                    break;
+                }
+            }
+            return bRet;
+        }
+
+        private string SerializeList(dynamic list)
+        {
+            StringBuilder sbResult = new StringBuilder();
+
+            sbResult.Append("[");
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i > 0)
+                {
+                    sbResult.Append(",");
+                }
+
+                sbResult.Append(SerializeValue(list[i]));
+            }
+            sbResult.Append("]");
+
+            return sbResult.ToString();
+        }
+
+        private string SerializeValue(dynamic value)
+        {
+            StringBuilder sbResult = new StringBuilder();
+
+            if (value != null)
+            {
+                Type _typ = value.GetType();
+                if (_typ == typeof(string))
+                {
+                    sbResult.Append(string.Concat("\"", value, "\""));
+                }
+                else if (_typ.IsGenericType &&
+                    _typ.GetGenericTypeDefinition() == typeof(List<>))
+                {
+                    sbResult.Append(SerializeList(value));
+                }
+                else
+                {
+                    sbResult.Append(value.ToString());
+                }
+            }
+            else
+            {
+                sbResult.Append("null");
+            }
+
+            return sbResult.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sbResult = new StringBuilder();
+
+            sbResult.Append("{");
+            
+            Dictionary<string, object>.Enumerator e = mValues.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (sbResult.Length > 2)
+                {
+                    sbResult.Append(",");
+                }
+
+                sbResult.Append(string.Concat("\"", e.Current.Key, "\":"));
+                sbResult.Append(SerializeValue(e.Current.Value));
+            }
+
+            sbResult.Append("}");
+
+            return sbResult.ToString();
         }
 
     }
--- UnitTest/UnitTest2.cs
+++ UnitTest/UnitTest2.cs
@@ -33,5 +33,56 @@ namespace UnitTest
             dynamic configObj = parser.AddJsonFile(path).AddJsonFile(path2).Parse();
             Assert.Equal(configObj.Test, "This is a example.");
         }
+
+        [Fact]
+        public void HasPath()
+        { 
+            string path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
+            Json2Dynamic parser = new Json2Dynamic();
+            dynamic configObj = parser.AddJsonFile(path).Parse();
+
+            bool ret = configObj.HasPath("User");
+            Assert.True(ret);
+            ret = configObj.HasPath("User.Name");
+            Assert.True(ret);
+
+            ret = configObj.HasPath("NotHave");
+            Assert.False(ret);
+            ret = configObj.HasPath("User.NotHave");
+            Assert.False(ret);
+        }
+
+        [Fact]
+        public void RemovePath()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
+            Json2Dynamic parser = new Json2Dynamic();
+            dynamic configObj = parser.AddJsonFile(path).Parse();
+
+            bool ret = configObj.HasPath("User.Name");
+            Assert.True(ret);
+            configObj.RemovePath("User.Name");
+            ret = configObj.HasPath("User.Name");
+            Assert.False(ret);
+
+            ret = configObj.HasPath("User");
+            Assert.True(ret);
+            configObj.RemovePath("User");
+            ret = configObj.HasPath("User");
+            Assert.False(ret);
+        }
+
+        [Fact]
+        public void SetValueByPath()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
+            Json2Dynamic parser = new Json2Dynamic();
+            dynamic configObj = parser.AddJsonFile(path).Parse();
+
+            Assert.Equal(configObj.User.Name, "Wangxm");
+
+            configObj.SetValueByPath("User.Name", "Huxy");
+            Assert.Equal(configObj.User.Name, "Huxy");
+        }
     }
 }
