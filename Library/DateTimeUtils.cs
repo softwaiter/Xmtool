a1d3d157d1a161ead37dffb1980d4e8d2d5ac8c0
softwaiter <softwaiter@126.com>
2021-07-14 20:50:12 +0800

    :sparkles:增加日期时间常用方法库

---
+++ Library/DateTimeUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeM.Common.Tools
+{
+    public class DateTimeUtils
+    {
+        public static long GetUtcTimestamp10()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+            return (long)ts.TotalSeconds;
+        }
+
+        public static long GetUtcTimestamp13()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+            return (long)ts.TotalMilliseconds;
+        }
+
+        public static DateTime GetLocalDateTimeFromUtcTimestamp10(long ts)
+        {
+            DateTime st = TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1, 0, 0, 0), TimeZoneInfo.Local);
+            return st.AddSeconds(ts);
+        }
+
+        public static DateTime GetLocalDateTimeFromUtcTimestamp13(long ts)
+        {
+            DateTime st = TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1, 0, 0, 0), TimeZoneInfo.Local);
+            return st.AddMilliseconds(ts);
+        }
+    }
+}
---
+++ UnitTest/UnitTest5.cs
@@ -0,0 +1,49 @@
+﻿using CodeM.Common.Tools;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTest
+{
+    public class UnitTest5
+    {
+        private ITestOutputHelper output;
+
+        public UnitTest5(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            DateTime dtNow = DateTime.Now;
+            long ts = DateTimeUtils.GetUtcTimestamp10();
+            Assert.True((ts + "").Length == 10);
+
+            DateTime dt = DateTimeUtils.GetLocalDateTimeFromUtcTimestamp10(ts);
+            Assert.Equal(dt.Year, dtNow.Year);
+            Assert.Equal(dt.Month, dtNow.Month);
+            Assert.Equal(dt.Day, dtNow.Day);
+            Assert.Equal(dt.Hour, dtNow.Hour);
+            Assert.Equal(dt.Minute, dtNow.Minute);
+            Assert.Equal(dt.Second, dtNow.Second);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            DateTime dtNow = DateTime.Now;
+            long ts = DateTimeUtils.GetUtcTimestamp13();
+            Assert.True((ts + "").Length == 13);
+
+            DateTime dt = DateTimeUtils.GetLocalDateTimeFromUtcTimestamp13(ts);
+            Assert.Equal(dt.Year, dtNow.Year);
+            Assert.Equal(dt.Month, dtNow.Month);
+            Assert.Equal(dt.Day, dtNow.Day);
+            Assert.Equal(dt.Hour, dtNow.Hour);
+            Assert.Equal(dt.Minute, dtNow.Minute);
+            Assert.Equal(dt.Second, dtNow.Second);
+        }
+    }
+}
