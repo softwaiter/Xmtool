3eedf11ffba53d2c1912be464f6a2380685f97b6
softwaiter <softwaiter@126.com>
2021-12-19 13:09:35 +0800

    :construction:调整验证码字体大小计算逻辑

--- Library/CaptchaTool.cs
+++ Library/CaptchaTool.cs
@@ -67,7 +67,7 @@ namespace CodeM.Common.Tools
             Pen penBorder = new Pen(sBorderColor, 1);
             g.DrawRectangle(penBorder, 0, 0, width - 1, height - 1);
 
-            float fontSize = width / (code.Length + 1);
+            float fontSize = height / 2.5f;
             Font font = new Font(SystemFonts.DefaultFont.FontFamily,
                 fontSize, FontStyle.Bold | FontStyle.Italic);
             SizeF charSize = g.MeasureString("G", font);
